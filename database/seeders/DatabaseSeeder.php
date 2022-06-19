6ba60d1750f7688146e225910789dbecdf94762f
imii <imiia75775@gmail.com>
2022-06-19 22:06:15 +0700

    Nambah Status dari Idea :D

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use App\Models\Idea;
+use App\Models\Status;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,6 +19,13 @@ class DatabaseSeeder extends Seeder
         Category::factory()->create(['name' => 'Development']);
         Category::factory()->create(['name' => 'Marketing']);
         Category::factory()->create(['name' => 'Sales']);
+
+        Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+        Status::factory()->create(['name' => 'Considering', 'classes' => 'bg-purple text-white']);
+        Status::factory()->create(['name' => 'In Progress', 'classes' => 'bg-yellow text-white']);
+        Status::factory()->create(['name' => 'Implemented', 'classes' => 'bg-green text-white']);
+        Status::factory()->create(['name' => 'Close', 'classes' => 'bg-red text-white']);
+
         Idea::factory(30)->create();
     }
 }
